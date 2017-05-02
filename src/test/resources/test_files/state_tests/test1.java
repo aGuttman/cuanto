c9c94e5af6421a795f834aa67f17ab8c1889007f
Shawn Meier <shawn.meier@colorado.edu>
2017-05-02 13:52:20 -0600

    tests for state tests

--- src/test/resources/test_files/state_tests/test1.java
+++ src/test/resources/test_files/state_tests/test1.java
@@ -1,4 +1,18 @@
 class test1{
 	public static void main(String[] args){
 	}
+	public static void foo(){
+		Object o = new Object();
+	}
+	public static void bar(){
+		Object o = new Object();
+		o.toString();
+	}
+	public static void baz(boolean b){
+		Object o = null;
+		if(b){
+			o = new Object();
+		}
+		o.toString();	
+	}
 }
